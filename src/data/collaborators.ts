630ff0e5af1beb3443bccf2abeb516fe2eef46e7
AbdulHadi <179337358+Abdulhadi446@users.noreply.github.com>
2026-04-15 16:17:50 +0500

    Add new avatar image for user trillioniar1

--- src/data/collaborators.ts
+++ src/data/collaborators.ts
@@ -5,10 +5,9 @@ const collaboratorsData: Collaborator[] = [
     id: "collab-ctbfq9",
     name: "Muhammad Zain",
     url: "https://www.linkedin.com/in/muhammad-zain-359626291/",
-    avatarUrl:
-      "https://media.licdn.com/dms/image/v2/D4D03AQH0oeWM-wgIFA/profile-displayphoto-scale_200_200/B4DZtTmyUHGgAY-/0/1766634237152?e=1777507200&v=beta&t=JuDtj9B8GXktxQdYmeSQ4zL4e1g8WstTOAXvrSpD7jQ",
+    avatarUrl: "/avatars/trillioniar1.jpeg",
     isPlaceholder: false,
-    order: 0
+    order: 0,
   },
   {
     id: "collab-qnrkae",
@@ -16,8 +15,10 @@ const collaboratorsData: Collaborator[] = [
     url: "https://github.com/arham-aftab001",
     avatarUrl: "https://avatars.githubusercontent.com/u/256279757?v=4",
     isPlaceholder: false,
-    order: 1
-  }
+    order: 1,
+  },
 ];
 
-export const collaborators = collaboratorsSchema.parse(collaboratorsData).sort((a, b) => a.order - b.order);
+export const collaborators = collaboratorsSchema
+  .parse(collaboratorsData)
+  .sort((a, b) => a.order - b.order);
